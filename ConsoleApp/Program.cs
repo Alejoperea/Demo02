52fa722bceade978e1189bb69e777db051abf810
Alejo Perea <mpereamejia@gmail.com>
2024-12-10 10:13:07 -0500

    Creacion Clase Employee

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,10 +2,20 @@
 
 using MyLibrary;
 
-int i = 5;
-string Name = "Hello";
+Secretary Maria = new Secretary();
 
+Maria.Name = "Maria";
 
-Product Leche;
-Leche = new Product(1,"Lecha Entera",19,100);
+Manager Pedro = new Manager();
+Pedro.Name = "Pedro";
 
+Developer Miguel = new Developer();
+
+WriteEmployeeInfo(Maria);
+WriteEmployeeInfo(Pedro);
+WriteEmployeeInfo(Miguel);
+
+void WriteEmployeeInfo (Employee employee)
+{
+	Console.WriteLine($"{employee.Name} : {employee.CalculateMonthlyPayment()}");
+}
