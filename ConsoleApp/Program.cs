395bb3a6207f6488d5a5edf0f0ca450efed7b95e
Alejo Perea <mpereamejia@gmail.com>
2024-12-10 10:37:53 -0500

    Configuracion final del Programa y la clase Employee

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,14 +2,16 @@
 
 using MyLibrary;
 
-Secretary Maria = new Secretary();
+Employee Maria = new Secretary();
 
 Maria.Name = "Maria";
 
-Manager Pedro = new Manager();
+Employee Pedro = new Manager();
 Pedro.Name = "Pedro";
 
 Developer Miguel = new Developer();
+Miguel.Name = "Miguel";
+Console.WriteLine($"Dias: {Miguel.GetPaymentDays()}");
 
 WriteEmployeeInfo(Maria);
 WriteEmployeeInfo(Pedro);
@@ -18,4 +20,5 @@ WriteEmployeeInfo(Miguel);
 void WriteEmployeeInfo (Employee employee)
 {
 	Console.WriteLine($"{employee.Name} : {employee.CalculateMonthlyPayment()}");
+	Console.WriteLine($"Days: {employee.GetPaymentDays()}");
 }
