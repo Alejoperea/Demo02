4b29a6cb0470afb4e17be0fe072a483af9becfa8
Alejo Perea <mpereamejia@gmail.com>
2024-12-09 22:28:52 -0500

    Inicio Proyecto

---
+++ MyLibrary/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+	public class Product
+
+	{
+		//Propiedades
+		
+		public string Name { get; set; }
+		public decimal UnitPrice { get; set; }
+		public int UnitsInStock { get; set; }
+
+	}
+}
